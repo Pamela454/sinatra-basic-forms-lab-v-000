229b5380612e8d146a9e29daafcb2c2f46e16a66
Pamela Frieze <pfrieze@gmail.com>
2018-07-05 00:06:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require_relative 'config/environment'
-
+#this is the controller
 class App < Sinatra::Base
+  get '/' do
+   erb :index
+  end
+
+  get "/new" do
+   erb :create_puppy
+  end
+
+  post '/' do
+    @puppy = Puppy.new
+  
+   erb :display_puppy
+  end
 
 end
